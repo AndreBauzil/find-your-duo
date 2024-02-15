59fcdf7c5104b7296af13311cade39cd55ecbfd8
AndreBauzil <Andregbauzil@gmail.com>
2024-02-14 23:57:09 -0300

    chore: add rules and themes for image and gradients

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.tsx'
+    './src/**/*.tsx',
+    './index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'gradient': 'linear-gradient(89.86deg, #9572FC 17.08%, #43E7AD 63.94%, #E1D55D 20.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 8%, rgba(0, 0, 0, 0.9) 67.88%)',
+      },
+    },
   },
   plugins: [],
 }
