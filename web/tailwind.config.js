2b0fad3cab13179707bac1b49006afb6f1db3304
AndreBauzil <Andregbauzil@gmail.com>
2024-02-15 00:38:18 -0300

    chore: add icons and font to project

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      },
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
         'gradient': 'linear-gradient(89.86deg, #9572FC 17.08%, #43E7AD 63.94%, #E1D55D 20.57%)',
