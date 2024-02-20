f5934b44ade342af6df28fcc8582ee2b7f65a749
Andre Bauzil <andregbauzil@gmail.com>
2024-02-20 10:26:59 -0300

    feat: finish structure for mobile app, with flatList for games, game card and styling for component

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png'
 
 import { styles } from './styles';
 import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/GameCard';
+import { GAMES } from '../../utils/games';
 
 export function Home() {
   return (
@@ -18,6 +19,20 @@ export function Home() {
         title="Encontre o seu duo!"
         subtitle="Selecione o game que deseja jogar..."
       />
+
+      <FlatList 
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GameCard 
+            data={item}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
+      />
+
     </View>
   );
 }
