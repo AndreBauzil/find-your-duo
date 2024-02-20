e45f99282bd224e2c8a5326594861d906d33b772
Andre Bauzil <andregbauzil@gmail.com>
2024-02-20 10:25:39 -0300

    feat: finish code for gameCard, add item structure and styling

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,11 +1,42 @@
-import { View } from 'react-native';
+import { TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType, Text } from 'react-native';
 
 import { styles } from './styles';
+import { LinearGradient } from 'expo-linear-gradient';
+import { THEME } from '../../theme';
 
-export function GameCard() {
+export interface GameCardProps{
+  id: string;
+  name: string;
+  ads: string;
+  cover: ImageSourcePropType;
+}
+
+interface Props{
+  data: GameCardProps;
+}
+
+
+export function GameCard({ data, ...rest }: Props) {
   return (
-    <View style={styles.container}>
+    <TouchableOpacity style={styles.container}>
+      <ImageBackground 
+        style={styles.cover}
+        source={data.cover}
+      >
+        <LinearGradient 
+          colors={THEME.COLORS.FOOTER}
+          style={styles.footer}
+        >
+          <Text style={styles.name}> 
+            {data.name}
+          </Text>
 
-    </View>
+          <Text style={styles.ads}> 
+            {data.ads} anúncios
+          </Text>
+
+        </LinearGradient>
+      </ImageBackground>
+    </TouchableOpacity>
   );
 }
