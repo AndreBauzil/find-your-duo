40552d9c7c705009d717c7d868e355824519c5a4
Andre Bauzil <andregbauzil@gmail.com>
2024-02-21 12:30:05 -0300

    feat: add prisma dependencies for queries on routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express'
+import { PrismaClient } from "@prisma/client";
 
 const app = express()
+const prisma = new PrismaClient({
+    log: ['query']
+})
 
 /* 
     Query: ?page=2&sort=title
@@ -8,30 +12,75 @@ const app = express()
     Body: safe and sensible info
 */
 
-// Listagem de games com contagem de anúncios
-app.get('/games', (request, response) => {
-    return response.json([])
+// List games with ads counting
+app.get('/games', async (request, response) => {
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true,
+                }
+            }
+        }
+    })
+
+
+    return response.json([games])
 })
-// Criação de novo anúncio
+
+// Creation new ad
 app.post('/ads', (request, response) => {
     return response.status(201).json([])
 })
-// Listagem de anúncios por game
-app.get('/games/:id/ads', (request, response) => {
+
+// List ads by game
+app.get('/games/:id/ads', async (request, response) => {
     const gameId = request.params.id;
     
-    return response.json([
-        { id: 1, name: 'Anuncio 1' },
-        { id: 2, name: 'Anuncio 2' },
-        { id: 3, name: 'Anuncio 3' },
-        { id: 4, name: 'Anuncio 4' },
-    ]);
+    // query to retrive ads without some attributes
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true,
+            yearsPlaying: true,
+            hourStart: true,
+            hourEnd: true,
+        },
+        where: {
+            gameId,
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+
+    // format before return
+    return response.json(ads.map(ad => {
+        return {
+            ...ads,
+            weekDays: ad.weekDays.split(',')
+        }
+    }));
 })
-// Buscar discord pelo ID do anúncio
-app.get('/ads/:id/discord', (request, response) => {
+
+// Search discord by ads ID
+app.get('/ads/:id/discord', async (request, response) => {
     const adId = request.params.id;
+
+    const ad = await prisma.ad.findUniqueOrThrow({
+        select: {
+            discord: true,
+        },
+        where: {
+            id: adId,
+        }
+    })
     
-    return response.json([])
+    return response.json({
+        discord: ad.discord,
+    })
 })
 
 app.listen(3333)   
