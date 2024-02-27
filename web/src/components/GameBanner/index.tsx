a43ac96d676762cf47c438c3ca59278aac77c0a6
AndreBauzil <Andregbauzil@gmail.com>
2024-02-27 18:34:50 -0300

    refactor: add to gameBanner component a component to radix work

--- web/src/components/GameBanner/index.tsx
+++ web/src/components/GameBanner/index.tsx
@@ -4,14 +4,14 @@ interface GameBannerProps {
     adsCount: number;
 }
 
-export default function GameBanner(props: GameBannerProps) {
+export function GameBanner(props: GameBannerProps) {
   return (
     <a className='relative rounded-lg overflow-hidden' href="">
         <img src={props.bannerUrl} alt="" />
 
         <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-        <strong className="font-bold text-white block">{props.title}</strong>
-        <span className="text-zinc-300 text-sm block">{props.adsCount} anúncios</span>
+          <strong className="font-bold text-white block">{props.title}</strong>
+          <span className="text-zinc-300 text-sm block">{props.adsCount} anúncios</span>
         </div>
     </a>
   )
